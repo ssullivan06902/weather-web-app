7a7f66a2e39fa943ef4d80e40af18c33be616b28
Sean S. <sean.m.a.sullivan@icloud.com>
2025-02-07 10:54:14 -0500

    chang api info

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
-const apiKey = '3bd1590d6c699874f3984157bc0a65a1'; // Replace with your actual OpenWeatherMap API key
+const apiKey = '756f34b557f409929fbc5dc11ca68e90'; // Replace with your actual OpenWeatherMap API key
 
 module.exports = apiKey;
